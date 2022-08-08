80237ac6a5a1c2b2cf608eb7274456b5260ff7e1
45ninjas <tom.parker1235@gmail.com>
2022-08-09 00:15:40 +0800

    Made the root get request return the readme.md

--- api/app.js
+++ api/app.js
@@ -8,6 +8,25 @@ const app = express();
 app.use(express.json());
 mountRoutes(app);
 
+// Send the readme.md file as a base64 string inside some html to render our markdown.
+app.get("/", (req, res) => {
+  res.send(`<!doctype html><html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Readme.md</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <style> body { grid-template-columns: 1fr min(80rem,90%) 1fr } blockquote p { margin: 0; } </style>
+  </head>
+  <body>
+    <div id="content"></div>
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+    <script>
+      document.getElementById('content').innerHTML =
+        marked.parse(atob('${require('fs').readFileSync("readme.md").toString('base64')}'));
+    </script>
+  </body></html>`);
+})
+
 // Start our app.
 app.listen(process.env.HTTP_PORT, () => {
   console.log(`PNH Auth. Listening on port ${process.env.HTTP_PORT}`)
