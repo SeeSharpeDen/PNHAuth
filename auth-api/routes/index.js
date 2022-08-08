78f55364a92686cb14953914de2044251b9c2cc9
45ninjas <tom.parker1235@gmail.com>
2022-08-08 14:06:12 +0800

    Added API key 'security'

--- auth-api/routes/index.js
+++ auth-api/routes/index.js
@@ -1,7 +1,20 @@
 const players = require("./players");
 const link = require("./link");
 
+// Only allows access when the correct API_KEY was sent.
+function checkKey(req, res, next) {
+
+    // Make sure that the Authorization is the same as the API_KEY
+    if(req.get("Authorization") !== process.env.API_KEY) {
+        return res.status(403).send("Invalid api key");
+    }
+    next();
+}
+
 module.exports = app => {
-    app.use('/v1/players', players)
+    // Require an API key for access to the players endpoint.
+    app.use('/v1/players', checkKey, players)
+
+    // Authentication is in the link_code so no checkKey middleware.
     app.use('/v1/link', link)
 };
