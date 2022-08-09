1f6f878c01129a2f6f9d89d950e3dfa3b3cb95bc
45ninjas <tom.parker1235@gmail.com>
2022-08-09 22:32:02 +0800

    web interface works!

--- www/js/register.js
+++ www/js/register.js
@@ -3,44 +3,106 @@ const api_url = "http://localhost:33084"
 // const link_code = document.querySelector('meta[data-link-code]').dataset.linkCode;
 const link_code = document.location.hash.substring(1);
 
+async function checkErrors(resp) {
+    if (!resp.ok && resp.status != 422) {
+        throw { status: resp.status, statusText: resp.statusText, body: await resp.text() }
+    }
+
+    return resp;
+}
 
 // Load the JSON from the API.
-fetch(api_url + '/v1/link/' + link_code).then(
-    async function (response) {
-        // document.getElementById("raw-data").innerText = response.;
-        if (!response.ok) {
-
-            // Show the error screen.
-            let error = document.getElementById("error" + response.status);
-            if (error == null) {
-                error = document.getElementById("error");
-                error.querySelector("h2").innerText = `${response.status}`
-                error.querySelector("p").innerText = `${await response.text()}`
-            }
-
-            error.removeAttribute("hidden");
-
-            // Hide the loading screen.
-            document.getElementById("loading").setAttribute("hidden", "");
-            return;
-        }
-
-        // Get the player data from the json.
-        let player_data = await response.json();
-
-        // Set the player profile
-        player_data["profile"] = "https://crafthead.net/avatar/" + player_data.uuid;
-
-        // Set the player data.
+fetch(api_url + '/v1/link/' + link_code)
+    .then(checkErrors)
+    .then((response) => response.json())
+    .then((player_data) => {
+        player_data['profile'] = `https://crafthead.net/avatar/${player_data.uuid}/180`;
         writePlayerData(player_data);
-        document.getElementById("loading").setAttribute("hidden", "");
-    }
-).catch(function (reason) {
-    throw reason
+        loadingPage(false);
+    })
+    .catch(handleError)
+// .finally(loadingPage(false))     this doesn't appear to work as expected.
+
+const button = document.querySelector("button");
+button.addEventListener('click', async function () {
+    loadingPage(true);
+    // Fill our data to send to the server.
+    let data = {};
+    document.querySelectorAll("form [data-player]").forEach(e => {
+        data[e.dataset.player] = e.value;
+    });
+
+    // Send the data to the url.
+    fetch(api_url + '/v1/link/' + link_code, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(handleInputError)
+        .then(checkErrors)
+        .then(() => {
+            document.getElementById("email-sent").removeAttribute("hidden");
+            loadingPage(false);
+        })
+        .catch(handleError)
+    // .finally(loadingPage(false))     this doesn't appear to work as expected.
 });
 
+function loadingPage(show) {
+    if (show != true)
+        document.getElementById("loading").setAttribute("hidden", "");
+    else
+        document.getElementById("loading").removeAttribute("hidden");
+}
+
+async function handleInputError(resp) {
+    if (resp.status == '422') {
+        const errors = await resp.json();
+
+        document.querySelectorAll("form [data-player]").forEach(e => {
+            let msg = errors[e.dataset.player];
+            if (msg != undefined && msg != "") {
+                e.parentElement.querySelector(".error").innerText = errors[e.dataset.player]
+                e.parentElement.classList.add("invalid")
+            } else {
+                e.parentElement.classList.remove("invalid")
+                e.parentElement.querySelector(".error").innerText = ""
+            }
+        })
+    }
+
+    return resp;
+}
+
+function handleError(error) {
+    // Error page for pre-defined http codes.
+    let error_page = document.getElementById('error-' + error.status);
+
+    if (error_page == null) {
+        error_page = document.getElementById('error');
+
+        // Error page for our custom HTTP errors.
+        if (error.status && error.statusText && error.body) {
+            error_page.querySelector("h3").innerText = `${error.status} - ${error.statusText}`;
+            error_page.querySelector("p").innerText = `${error.body}`;
+
+            // Error page for generic errors.
+        } else {
+            error_page.querySelector("h3").innerText = "That's Very Bad";
+            error_page.querySelector("p").innerText = `${error}`;
+
+            console.error(error);
+        }
+    }
+
+    // Show the error.
+    error_page.removeAttribute("hidden");
+}
+
 // Write the player's data to all elements with the data-player attribute.
-function writePlayerData(player) {
+async function writePlayerData(player) {
     document.querySelectorAll("[data-player]").forEach(e => {
         switch (e.tagName) {
             case 'IMG':
@@ -53,12 +115,5 @@ function writePlayerData(player) {
                 e.innerText = player[e.dataset.player];
                 break;
         }
-    })
-}
-
-// Write form errors to the form.
-function writeFormErrors(errors) {
-    document.querySelectorAll("form [data-player]").forEach(e => {
-        e.parentElement.querySelector(".error").innerText = errors[e.dataset.error]
     })
 }
